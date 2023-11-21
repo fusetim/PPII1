9d53344e86ee7c2b97393fe493e3246d48f26f41
FuseTim <fusetim@gmx.com>
2023-11-21 18:05:19 +0100

    fix(script): sql COPY FROM FILE is forbidden on remote server

--- scripts/Script_ingredients.py
+++ scripts/Script_ingredients.py
@@ -1,12 +1,9 @@
-import psycopg2 
-  
-conn = psycopg2.connect(database="PPII1", 
-                        user='postgres', password='1234',  
-                        host='127.0.0.1', port='5432'
-) 
+import psycopg
+
+conn = psycopg.connect("postgresql://ppii1@localhost:5432/ppii1")
 
 conn.autocommit = True
-cursor = conn.cursor() 
+cursor = conn.cursor()
 
 
 sql = '''create temporary table t (code varchar(10) PRIMARY KEY,x2 varchar(200),x3 varchar(200),x4 varchar(200), name text,x6 varchar(200),x7 varchar(200),x8 varchar(200),x9 varchar(200),x10 varchar(200),x11 varchar(200),x12 varchar(200),x13 varchar(200),co2 float,x15 varchar(200), x16 varchar(200),x17 varchar(200),x18 varchar(200),x19 varchar(200),x20 varchar(200),x21 varchar(200),x22 varchar(200),x23 varchar(200),x24 varchar(200),x25 varchar(200),x26 varchar(200),x27 varchar(200),x28 varchar(200),x29 varchar(200))'''
@@ -14,13 +11,17 @@ sql = '''create temporary table t (code varchar(10) PRIMARY KEY,x2 varchar(200),
 cursor.execute(sql)
 
 
-sql2 = '''copy t (code ,x2 ,x3 ,x4, name  ,x6 ,x7 ,x8 ,x9 ,x10 ,x11 ,x12 ,x13, co2  ,x15 ,x16 ,x17 ,x18 ,x19 ,x20 ,x21 ,x22 ,x23 ,x24 ,x25 ,x26 ,x27 ,x28, x29 )
-from '/home/tncy/Documents/Projet/venv/Agribalyse_Synthese.csv'
-DELIMITER ',' 
-CSV HEADER;'''
+sql2 = '''
+copy t (code ,x2 ,x3 ,x4, name  ,x6 ,x7 ,x8 ,x9 ,x10 ,x11 ,x12 ,x13, co2  ,x15 ,x16 ,x17 ,x18 ,x19 ,x20 ,x21 ,x22 ,x23 ,x24 ,x25 ,x26 ,x27 ,x28, x29 )
+from stdin
+DELIMITER ','
+CSV HEADER;
+'''
 
-cursor.execute(sql2)
-
+with open('agribalyse-31-synthese.csv', 'r') as f:
+    with cursor.copy(sql2) as copy:
+        for line in f:
+            copy.write(line)
 
 sql3 = '''drop table if exists ingredients'''
 
@@ -46,6 +47,4 @@ cursor.execute(sql6)
 
 
 conn.commit()
-conn.close() 
-
-  
+conn.close()
