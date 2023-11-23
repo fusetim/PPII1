779211ed2f60340f090c955c5d21cac24b3f2e21
FuseTim <62183512+fusetim@users.noreply.github.com>
2023-11-23 18:25:31 +0100

    feat(DB): finalizing the DB mappings (#10)
    * feat(DB): Recipes added
    * feat(DB): ingredient links and quantity types
    * doc: DB schematics

---
+++ backend/models/ingredient_link.py
@@ -0,0 +1,29 @@
+from db import db
+from sqlalchemy import String, Float, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class IngredientLink(db.Model):
+    """
+    IngredientLink data model.
+    It is a symbolic reference to an ingredient in a recipe.
+    It contains the quantity of the ingredient.
+
+    Attributes:
+        link_uid: The IngredientLink unique identifier.
+        recipe_uid: The recipe unique identifier.
+        ingredient_code: The ingredient identifier.
+        quantity: The quantity of the ingredient.
+        quantity_type_uid: The UUID of the type of quantity (mass, volume, etc.)
+        reference_quantity: The reference quantity (always in kg), that is used to compute the
+        equivalent Co2 emission. If NULL, the reference quantity should be computed using the
+        quantity_type (and use a conversion mechanism) and the quantity.
+    """
+    __tablename__ = "ingredient_links"
+    link_uid: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    recipe_uid: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    ingredient_code: Mapped[str] = mapped_column(String(length=10), nullable=False)
+    quantity: Mapped[float] = mapped_column(Float, nullable=False)
+    quantity_type_uid: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    reference_quantity: Mapped[float] = mapped_column(Float, nullable=True)
---
+++ backend/models/recipe.py
@@ -0,0 +1,39 @@
+from db import db
+from sqlalchemy import String, Float, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Recipe(db.Model):
+    """
+    Recipe data model.
+
+    Attributes:
+        recipe_uid: The recipe unique identifier.
+        name: The recipe name.
+        short_description: A short description of the recipe, displayed when browsing recipes.
+        description: A longer description of the recipe, displayed when viewing the recipe. It must include
+            the actual recipe instructions.
+        type: The type of recipe. This is a string that can be used to categorize recipes.
+        author: The author of the recipe. This is a UUID that can be used to link to the author's profile.
+    """
+    __tablename__ = "recipes"
+    recipe_uid: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(Text, nullable=False)
+    short_description : Mapped[str] = mapped_column(Text, nullable=False)
+    description: Mapped[str] = mapped_column(Text)
+    type: Mapped[str] = mapped_column(String(length = 30))
+    author: Mapped[UUID] = mapped_column(UUID(as_uuid=True), nullable=True)
+
+
+    def to_dict(self):
+        rv = dict()
+        rv['type'] = 'recipe'
+        rv['recipe_uid'] = self.recipe_uid 
+        rv['name'] = self.name 
+        rv['short_description'] = self.short_description
+        rv['description'] = self.description
+        rv['type'] = self.type
+        rv['author'] = self.author
+        return rv
+
