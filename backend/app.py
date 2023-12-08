c9cd303722c06ab2f9350da43a7d8b2536a7d864
BANAK Sacha <sacha.banak@telecomnancy.eu>
2023-12-08 23:35:42 +0100

    la recherche des ingredients fonctionne (voir sur la route /accueil)

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from api import api
 from flask import Flask, render_template, request, redirect
 import tomllib
+from sqlalchemy import text
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import models
@@ -8,7 +9,8 @@ from flask import g
 import os
 import sys
 from db import db
-from search_table import save_search_tables
+from search_table import save_search_tables, get_ingredient_table
+from lsh import normalize_str
 
 # Add the root directory to the PYTHONPATH
 p = os.path.abspath(".")
@@ -40,8 +42,17 @@ def hello_world():
 def accueil():
     if request.method == "POST":
         search = request.form["search"]
-        return redirect("/api/ingredients/search/"+search)
+        table = get_ingredient_table()
+        normalized_query = normalize_str(search)
+        codes = table.get(normalized_query, 10)
+        data = []
+        if codes == []:
+            return render_template("aucun_res.html")
+        else:
+            for code in codes:
+                #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
+                ingr = db.session.execute(text("SELECT name, co2 FROM ingredients WHERE code = :c"), {'c' : code}).all()[0]
+                data.append(ingr)
+            return render_template("res_ingredients.html", data=data)
     if request.method == "GET":
         return render_template("accueil.html")
-
-
