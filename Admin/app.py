5bd1e20c9077be8208d2c8433826ec268afee3c4
guillaume <guillaume.hisleur@telecomnancy.eu>
2023-12-10 15:52:47 +0100

    Pages de recherche recettes et ajout (rien d'installé à l'intérieur, juste une apparence)

---
+++ Admin/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/admin")
+def admin_search():
+    return render_template("admin_search.html")
+"""
+@app.route("/admin", Methods=['GET'])
+def admin_search_get():
+    return render_template("admin_search.html")"""
+
+@app.route("/admin/add")
+def admin_add():
+    return render_template("admin_add.html")
+
+@app.route("/admin/add", methods=['GET'])
+def admin_add_get():
+    return render_template("admin_add.html")
