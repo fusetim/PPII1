e6cc503b3604e3191b2aec624b5221d7eddb4d10
BANAK Sacha <sacha.banak@telecomnancy.eu>
2023-12-13 14:43:09 +0100

    docstrings sur les routes et la route 'result_recipes' n'est plus effective

--- backend/app.py
+++ backend/app.py
@@ -39,26 +39,18 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/home", methods=("GET",))
-def accueil():
-    if request.method == "POST":
-        search = request.form["search"]
-        table = get_ingredient_table()
-        normalized_query = normalize_str(search)
-        codes = table.get(normalized_query, 10)
-        data = []
-        if codes == []:
-            return render_template("aucun_res.html")
-        else:
-            for code in codes:
-                #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
-                ingr = db.session.execute(text("SELECT name, co2 FROM ingredients WHERE code = :c"), {'c' : code}).all()[0]
-                data.append(ingr)
-            return render_template("result_ingredients.html", data=data)
-    if request.method == "GET":
-        return render_template("home.html")
+def home():
+    """
+    home page with an "about" section and a search bar to question the "ingredients" db
+    """
+    return render_template("home.html")
 
-@app.route("/search_ingredients")
-def search():
+@app.route("/result_ingredients")
+def result_ingredients():
+    """
+    a page showing the results of the research from the home page
+    shows ingredients' names and co2 equivalent cost
+    """
     search = request.args.get("search")
     table = get_ingredient_table()
     normalized_query = normalize_str(search)
@@ -73,23 +65,27 @@ def search():
             data.append(ingr)
         return render_template("result_ingredients.html", data=data)
 
-"""
 @app.route("/recipes", methods=("GET",))
-def recettes():
-    if request.method == "POST":
-        search = request.form["search"]
-        table = get_recipe_table()
-        normalized_query = normalize_str(search)
-        codes = table.get(normalized_query, 10)
-        data = []
-        if codes == []:
-            return render_template("aucun_res.html")
-        else:
-            for code in codes:
-                #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
-                ingr = db.session.execute(text("SELECT name, co2 FROM ingredients WHERE code = :c"), {'c' : code}).all()[0]
-                data.append(ingr)
-            return render_template("res_recettes.html", data=data)
-    if request.method == "GET":
-        return render_template("recettes.html")
+def recipes():
+    """
+    a page showing a search bar to question the "recipes" db
+    """
+    return render_template("recipes.html")
+"""
+@app.route("/result_recipes", methods=("GET",))
+def result_recipes():
+    search = request.args.get("search")
+    table = get_recipe_table()
+    normalized_query = normalize_str(search)
+    codes = table.get(normalized_query, 10)
+    data = []
+    if codes == []:
+        return render_template("no_result.html")
+    else:
+        for code in codes:
+            #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
+            ingr = db.session.execute(text("SELECT name FROM Recipe WHERE recipe_uid = :c"), {'c' : code}).all()[0]
+            data.append(ingr)
+        #return render_template("result_recipes.html", data=data)
+        return str(ingr)
 """
