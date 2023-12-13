a230069cd884e2c94c6abee801f3e50d9a427957
BANAK Sacha <sacha.banak@telecomnancy.eu>
2023-12-13 15:00:59 +0100

    mauvaise route pour la recherche + clean-up

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,12 @@
 from api import api
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import tomllib
 from sqlalchemy import text
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import models
-from flask import g
 import os
 import sys
 from db import db
-from search_table import save_search_tables, get_recipe_table, get_ingredient_table
+from search_table import save_search_tables, get_ingredient_table
 from lsh import normalize_str
 
 # Add the root directory to the PYTHONPATH
@@ -38,6 +35,7 @@ app.teardown_appcontext(save_search_tables)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/home", methods=("GET",))
 def home():
     """
@@ -45,7 +43,8 @@ def home():
     """
     return render_template("home.html")
 
-@app.route("/result_ingredients")
+
+@app.route("/search_ingredients")
 def result_ingredients():
     """
     a page showing the results of the research from the home page
@@ -60,32 +59,17 @@ def result_ingredients():
         return render_template("no_result.html")
     else:
         for code in codes:
-            #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
-            ingr = db.session.execute(text("SELECT name, co2 FROM ingredients WHERE code = :c"), {'c' : code}).all()[0]
+            # .all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
+            ingr = db.session.execute(
+                text("SELECT name, co2 FROM ingredients WHERE code = :c"), {"c": code}
+            ).all()[0]
             data.append(ingr)
         return render_template("result_ingredients.html", data=data)
 
-@app.route("/recipes", methods=("GET",))
+
+@app.route("/recipes")
 def recipes():
     """
     a page showing a search bar to question the "recipes" db
     """
     return render_template("recipes.html")
-"""
-@app.route("/result_recipes", methods=("GET",))
-def result_recipes():
-    search = request.args.get("search")
-    table = get_recipe_table()
-    normalized_query = normalize_str(search)
-    codes = table.get(normalized_query, 10)
-    data = []
-    if codes == []:
-        return render_template("no_result.html")
-    else:
-        for code in codes:
-            #.all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
-            ingr = db.session.execute(text("SELECT name FROM Recipe WHERE recipe_uid = :c"), {'c' : code}).all()[0]
-            data.append(ingr)
-        #return render_template("result_recipes.html", data=data)
-        return str(ingr)
-"""
