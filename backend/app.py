5a69960387edb7733754acf5b03c0450a81fc322
SachaBnk <82036135+SachaBnk@users.noreply.github.com>
2023-12-13 15:07:59 +0100

    feat(frontend): homepage & ingredient search (#15)
    Goals:
    * A first step towards a complete frontend, this commit allows #13 to be integrated and merged in the near future.
    * Design/Styles is still to do and is the next step
    * Recipe search has to be implemented (but will follow approximatively the same structure)
    
    Subcommits:
    * création de frontend/accueil.html
    * la barre de recherche envoie bien l'input à flask (le dossier templates est dans backend)
    * envoi de l'input de la barre de recherche
    * essai d'affichage des resultats de la recherche
    * la recherche des ingredients fonctionne (voir sur la route /accueil)
    * ratio apple 1
    * correction : creation de la route search (search est en query parameter)
    * changement des noms
    * suggestions by fusetim (cf #15)
    * docstrings sur les routes et la route 'result_recipes' n'est plus effective
    * mauvaise route pour la recherche + clean-up
    
    
    
    Co-authored-by: BANAK Sacha <sacha.banak@telecomnancy.eu>
    Co-authored-by: FuseTim <62183512+fusetim@users.noreply.github.com>
    Refs: #15, #13
    Closes: #15

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,13 @@
 from api import api
-from flask import Flask
+from flask import Flask, render_template, request
 import tomllib
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from flask_migrate import Migrate
-import models
-from flask import g
 import os
 import sys
 from db import db
-from search_table import save_search_tables
+from search_table import save_search_tables, get_ingredient_table
+from lsh import normalize_str
 
 # Add the root directory to the PYTHONPATH
 p = os.path.abspath(".")
@@ -35,3 +34,42 @@ app.teardown_appcontext(save_search_tables)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/home", methods=("GET",))
+def home():
+    """
+    home page with an "about" section and a search bar to question the "ingredients" db
+    """
+    return render_template("home.html")
+
+
+@app.route("/search_ingredients")
+def result_ingredients():
+    """
+    a page showing the results of the research from the home page
+    shows ingredients' names and co2 equivalent cost
+    """
+    search = request.args.get("search")
+    table = get_ingredient_table()
+    normalized_query = normalize_str(search)
+    codes = table.get(normalized_query, 10)
+    data = []
+    if codes == []:
+        return render_template("no_result.html")
+    else:
+        for code in codes:
+            # .all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
+            ingr = db.session.execute(
+                text("SELECT name, co2 FROM ingredients WHERE code = :c"), {"c": code}
+            ).all()[0]
+            data.append(ingr)
+        return render_template("result_ingredients.html", data=data)
+
+
+@app.route("/recipes")
+def recipes():
+    """
+    a page showing a search bar to question the "recipes" db
+    """
+    return render_template("recipes.html")
