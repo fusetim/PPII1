851b798cc5fe166dc61d0bddc5fc75a1fb2340d0
BANAK Sacha <sacha.banak@telecomnancy.eu>
2023-12-20 17:08:18 +0100

    la page recettes fonctionne (note : l'affichage est toujours moche, ça arrive - note 2 : les pages no result arrivent aussi)

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 import os
 import sys
 from db import db
-from search_table import save_search_tables, get_ingredient_table
+from search_table import save_search_tables, get_ingredient_table, get_recipe_table
 from lsh import normalize_str
 
 # Add the root directory to the PYTHONPATH
@@ -55,7 +55,10 @@ def result_ingredients():
     normalized_query = normalize_str(search)
     codes = table.get(normalized_query, 10)
     data = []
-    if codes == []:
+    other = db.session.execute(
+                text(f"SELECT name, co2 FROM ingredients WHERE normalized_name LIKE '%{normalized_query}%'")
+            ).all()
+    if codes == [] and other == []:
         if search != "":
             return render_template("no_result_ingredients.html")
         else:
@@ -64,15 +67,45 @@ def result_ingredients():
         for code in codes:
             # .all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
             ingr = db.session.execute(
-                text("SELECT normalized_name, co2 FROM ingredients WHERE code = :c"), {"c": code}
+                text("SELECT name, co2 FROM ingredients WHERE code = :c"), {"c": code}
             ).all()[0]
             data.append(ingr)
-        return render_template("result_ingredients.html", data=data, search=search)
+        if search != "":
+            for r in other:
+                if r not in data:
+                    data.append(r)
+        return render_template("result_ingredients.html", data=data[:20], search=search)
 
 
-@app.route("/recipes")
+@app.route("/search_recipes")
 def recipes():
     """
     a page showing a search bar to question the "recipes" db
     """
-    return render_template("recipes.html")
+    search = request.args.get("search")
+    table = get_recipe_table()
+    normalized_query = normalize_str(search)
+    codes = table.get(normalized_query, 10)
+    data = []
+    other = db.session.execute(
+                text(f"SELECT name FROM recipes WHERE name LIKE '%{search}%'")
+            ).all()
+    if codes == [] and other == []:
+        if search != "":
+            return render_template("no_result_recipes.html")
+        else:
+            return render_template("result_recipes.html", data=data, search=search)
+    else:
+        for code in codes:
+            # .all to get the list of sql outputs and [0] to get the tuple str-int (the output is a singleton)
+            recipe = db.session.execute(
+                text("SELECT name FROM recipes WHERE code = :c"), {"c": code}
+            ).all()[0]
+            # on veut maintenant chercher les resultats contenant search
+            data.append(recipe)
+
+        if search != "":
+            for r in other:
+                if r not in data:
+                    data.append(r)
+        return render_template("result_recipes.html", data=data[:20], search=search)
