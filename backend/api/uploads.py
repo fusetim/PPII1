941ad7217c15cef60f031e48adc05d8334b336db
FuseTim <fusetim@gmx.com>
2023-12-22 16:31:06 +0100

    feat: small changes

--- backend/api/uploads.py
+++ backend/api/uploads.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 from models.upload import Upload
 from db import db
 from flask_login import login_required, current_user
-from util.upload_helper import allowed_file, file_parts, cid_of_file, get_upload_dir
+from util.upload_helper import allowed_file, file_parts, cid_of_file, get_upload_dir, get_upload_url
 import os
 from os.path import exists as path_exists
 from werkzeug.utils import secure_filename
@@ -42,6 +42,7 @@ def add():
         if content_id is None:
             return jsonify({"error": "Invalid file."}), 400
         (stem, extension) = file_parts(filename)
+        upload = None
         try:
             os.rename(save_path, os.path.join(get_upload_dir(), f"{content_id}.{extension}"))
             upload = Upload(
@@ -55,5 +56,5 @@ def add():
         except:
             db.session.rollback()
             return jsonify({"error": "An error occured while uploading the file."}), 500
-        return jsonify({"content_id": content_id}), 200
+        return jsonify({"cid": content_id, "upload_uid": upload.upload_uid, "url": get_upload_url(upload)}), 200
     return jsonify({"error": "Unsupported file extension."}), 400
