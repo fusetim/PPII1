7802bed39dc8f33e1d1e89fe99eed3f7ce6904e7
Clement Escude--Cotinat <clement.escude--cotinat@telecomnancy.eu>
2023-12-27 12:11:54 +0100

    feat(backend/api/admin): implement recipe deletion (for real this time🤔)

--- backend/api/admin/recipes_admin.py
+++ backend/api/admin/recipes_admin.py
@@ -6,5 +6,21 @@ from models.ingredient_link import IngredientLink
 from models.quantity_type import QuantityType
 from db import db
 
+from recipes import RecipeNotFound, not_found
+
 # Creates the recipes "router" (aka blueprint in Flask)
 bp = Blueprint("recipes", __name__)
+
+
+@bp.route("/delete/<uuid:id>", methods=["DELETE"])
+def delete_recipe(id):
+    """
+    Delete recipe with uuid = id. Associated ingredient
+    links will be deleted too.
+    """
+    recipe_to_delete = Recipe.query.get(id)
+    if recipe is None:
+        raise RecipeNotFound(context="Not in DB.", payload={"code": id})
+
+    db.session.delete(recipe_to_delete)
+    db.session.commit()
